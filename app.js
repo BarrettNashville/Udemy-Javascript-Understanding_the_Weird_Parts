0d8f978897b61bdf67117ec2e259bf4e167c23be
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-17 15:39:04 -0500

    Lecture 20: Primitive Types. List of the 6 primitive types used in JS.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//long running function
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while(new Date() < ms) {}
-    console.log('finished function');
-}
+// Primitive Type: A type of data that represents a single value.
+// Meaning... it's not an object. 
 
-function clickHandler() {
-    console.log('click event!');
-}
+// There are 6 primitive types in JavaScript: 
 
-//listen for the click event
-document.addEventListener('click', clickHandler);
+// Undefined: represents a lack of existence. Don't use. This is what JS sets all variables to in the creation phase.
 
-waitThreeSeconds();
-console.log('finished execution');
+// Null: represents a lack of existence. You can set a variable to this. 
+
+// Boolean: true or false. 
+
+// Number: only one numeric type. It's a floating point number meaning there are always some decimals attached to it. This can make math weird. 
+
+// String: a sequence of characters. Both '' and "" can be used to surround a string.
+
+// Symbol: (new: used in ES6) We won't talk about this here. 
