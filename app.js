915faf077dda158b4c5617ef4517ac2662ebd75a
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 16:20:21 -0500

    Lecture 35: Function Statements, Function Expressions, First Class Functions

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
+greet();
+
+//Function Statement (doesn't return a value)
 function greet() {
     console.log('hi');
 }
 
-greet.language = 'english';
+//Function Expression (returns the function object)
+var anonymousGreet = function() {
+    console.log('hi');
+}
 
-console.log(greet.language);
+anonymousGreet();
 
+function log(a) {
+    console.log(a);
+}
+
+log(3);
+log("hello");
+log({
+   greeting: 'hi' 
+});
+log(function() {
+   console.log('hi'); 
+});
+
+// instead of displaying the function definiton, lets invoke it
+
+// Passing a function as a parameter
+function log2(a) {
+  a();  
+};
+
+log2(function() {
+   console.log('hi'); 
+});
