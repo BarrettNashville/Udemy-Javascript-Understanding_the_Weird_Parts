986665143181911a9a253ea29943f73865bc1f78
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 15:58:14 -0500

    Lecture 34: Functions are Objects and can have properties assigned to them

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-var objectLiteral = {
-    firstname: 'Mary',
-    isAProgrammer: true
+function greet() {
+    console.log('hi');
 }
 
-// JSON properties have to be wrapped in quotes
-// in actual JavaScript, you can do this but don't have to
-// JSON is technically a subset of object literal syntax
-// anything that is JSON-valid is valid OLS, but not all OLS is valid JSON
+greet.language = 'english';
 
-/*
-{
-    "firstname": "Mary",
-    "isAProgrammer": true
-}
-*/
+console.log(greet.language);
 
-// Built into JavaScript is a way to convert an object into JSON using JSON.stringify()
-
-console.log(JSON.stringify(objectLiteral));
-
-// You can also do the reverse using JSON.parse()
-
-var jsonValue = JSON.parse('{"firstname": "Mary","isAProgrammer": true}');
-
-console.log(jsonValue);
