3979a39d7e5433cd8e771d406f1e4fab18022378
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 17:35:44 -0500

    Lecture 40: A simple way to mimic function overloading, often found in frameworks

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 function greet(firstname, lastname, language) {
-    
-    // you can set default parameters like so.
-    // in ES6, you can do this by setting a parameter = value 
     language = language || 'en';
     
-    //to do nothing if no args passed:
-    
-    if(arguments.length === 0) {
-        console.log('Missing Parameters!');
-        console.log('-----------');
-        return;
+    if (language === 'en') {
+        console.log('Hello ' + firstname + ' ' + lastname);
     }
     
-    console.log(firstname);
-    console.log(lastname);
-    console.log(language);
-    console.log(arguments);
-    console.log(arguments[0]);
-    console.log('-----------');
-    
-    // arguments is array-like. its not actually an array but it does look and act like one in some ways
-    
-    // arguments will eventually be deprecated in favor of a spread parameter
-    // so the function signature might look like this:
-    //function greet(firstname, lastname, language, ...other)
-    //and when calling greet, all extra parameters get wrapped up in an array called "other" in this case
+    if (language === 'es') {
+        console.log('Hola ' + firstname + ' ' + lastname);
+    }
 }
 
-greet();
-greet('John');
-greet('John', 'Doe');
-greet('John', 'Doe', 'en');
+function greetEnglish(firstname, lastname) {
+    greet(firstname, lastname, 'en');
+}
+
+function greetSpanish(firstname, lastname) {
+    greet(firstname, lastname, 'es');
+}
+
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
