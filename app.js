2f1a5c33a281a239daa4b5f5353ca8cbdde1d546
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 15:21:31 -0500

    Lecture 30 - Objects and the Dot. Accessing object members with the dot operator and bracket notation.

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-//If you are using two library files in your project, a lib1.js and a lib2.js and each has code that sets libraryName = "Lib1" or "Lib2", then the last one loaded wins. However, if you don't want to override a variable if it is already set, then you can use the or operator trick like so:
+var person = new Object();
 
-window.libraryName = window.libraryName || "Lib 2";
+person["firstname"] = "Tony";
+person["lastname"] = "Alicea";
 
-// Usually libraries use very few global values, so if you load a library and can't use it, then you know you've probably overridden a variable name used in that library accidentally. 
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+
+person.address.street = "111 Main St.";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
