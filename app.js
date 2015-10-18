1026618075ca5fa2d03c40758d6daf848b5316fc
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 17:26:53 -0500

    Lecture 39: "arguments" and the forthcoming spread parameter

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-// this is the Array Literal Syntax, similar to the Object Literal Syntax
+function greet(firstname, lastname, language) {
+    
+    // you can set default parameters like so.
+    // in ES6, you can do this by setting a parameter = value 
+    language = language || 'en';
+    
+    //to do nothing if no args passed:
+    
+    if(arguments.length === 0) {
+        console.log('Missing Parameters!');
+        console.log('-----------');
+        return;
+    }
+    
+    console.log(firstname);
+    console.log(lastname);
+    console.log(language);
+    console.log(arguments);
+    console.log(arguments[0]);
+    console.log('-----------');
+    
+    // arguments is array-like. its not actually an array but it does look and act like one in some ways
+    
+    // arguments will eventually be deprecated in favor of a spread parameter
+    // so the function signature might look like this:
+    //function greet(firstname, lastname, language, ...other)
+    //and when calling greet, all extra parameters get wrapped up in an array called "other" in this case
+}
 
-var myArray = [1, 2, 3];
-
-// arrays can hold items of any type
-
-var arr = [
-    1,
-    false,
-    {
-        name: 'Barrett',
-        address: '111 Main St.'
-    },
-    function(name){
-        var greeting = 'Hello ';
-        console.log(greeting + name);
-    },
-    "hello"
-];
-
-console.log(arr);
-arr[3](arr[2].name);
+greet();
+greet('John');
+greet('John', 'Doe');
+greet('John', 'Doe', 'en');
