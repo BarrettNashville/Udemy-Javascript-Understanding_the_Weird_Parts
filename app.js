bf5308b32ed5eba864d23216ca4b6eeff37bf2d9
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 15:30:55 -0500

    Lecture 31: Using the Object Literal Syntax

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-var person = new Object();
+var Barrett = { 
+    firstname:'Barrett', 
+    lastname:'Cunningham',
+    address:{
+        street: '123 Main St',
+        city: 'Nashville',
+        state: 'TN'
+    }
+};
 
-person["firstname"] = "Tony";
-person["lastname"] = "Alicea";
+function greet(person) {
+    console.log('Hi ' + person.firstname);
+}
 
-var firstNameProperty = "firstname";
+greet(Barrett);
 
-console.log(person);
-console.log(person[firstNameProperty]);
+greet({ 
+    firstname: 'Mary', 
+    lastname: 'Doe' 
+});
 
-console.log(person.firstname);
-console.log(person.lastname);
-
-person.address = new Object();
-
-person.address.street = "111 Main St.";
-person.address.city = "New York";
-person.address.state = "NY";
-
-console.log(person.address.street);
-console.log(person.address.city);
-console.log(person["address"]["state"]);
+Barrett.address2 = {
+    street: '333 Second St.'
+}
