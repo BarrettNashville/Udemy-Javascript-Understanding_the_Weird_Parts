923e22fb86d25d43a60568d28366e7e0432e882a
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-19 12:23:31 -0500

    Lecture 51: Functional Programming

--- app.js
+++ app.js
@@ -1,84 +1,54 @@
-var person = {
-    firstname: 'John',
-    lastname: 'Doe', 
-    getFullName: function() {
-        var fullname = this.firstname + ' ' + this.lastname;
-        return fullname;
-    }
+var arr1 = [1, 2, 3];
+console.log(arr1);
+
+// to create a copy without functional programming doubling each value
+var arr2 = [];
+for(var i = 0; i < arr1.length; i++) {
+    arr2.push(arr1[i] * 2);
+}
+console.log(arr2);
+
+// with functional programming
+
+function mapForEach(arr, fn) {
+    var newArr = [];
+    for(var i = 0; i < arr1.length; i++) {
+        newArr.push(
+            fn(arr[i])
+        )
+    };
+    return newArr;
 }
 
-var logName = function(lang1, lang2) {
-    console.log('Logged: ' + this.getFullName());
-    // fails if you call logname(); directly because "this" is pointing to the global object
+var arr3 = mapForEach(arr1, function(item){
+   return item * 2; 
+});
+console.log(arr3);
+
+// do something else to the array using the same mapForEach function 
+var arr4 = mapForEach(arr1, function(item){
+   return item > 2; 
+});
+console.log(arr4);
+
+// create a function to use with mapForEach that allows you to pass a variable which it uses conditionally 
+
+var checkPastLimit = function(limiter, item) {
+    return item > limiter;
 }
 
-var logPersonName = logName.bind(person);
-// there are three built in methods available to every function object: call(), apply(), and bind()
-// bind() allows you to pass whatever object you want to be the "this" variable when you run the function. the bind() method returns a new function, it actually makes a copy of the logName function in this case and does some special stuff behind the scenes so that "this" points by reference to the passed object
+var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+console.log(arr4);
 
-logPersonName();
+// an easier way without having to use bind every time
 
-//you can immediately call the bind method when assigning the function to the variable
-
-var logName2 = function(lang1, lang2) {
-    console.log('Logged: ' + this.getFullName());
-}.bind(person);
-
-logName2();
-
-
-//
-
-var logName3 = function(lang1, lang2) {
-    console.log('Logged: ' + this.getFullName());
-    console.log('Arguments: ' + lang1 + ' ' + lang2);
-    console.log('---------');
+var checkPastLimitSimplified = function(limiter) {
+    return function(limiter, item) {
+        return item > limiter;
+    }.bind(this, limiter);
 }
 
-var logPersonName2 = logName3.bind(person);
+var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
+console.log(arr5);
 
-logPersonName2('en');
-
-// call() calls the function, but it lets you bind to the "this" variable using the parameter passed. bind creates a copy of the function with the "this" variable bound, but it does not call the function. 
-
-logPersonName2.call(person, 'en', 'es');
-
-// apply() is the same as call() except it wants an array of parameters. thats the only difference. this can be useful for mathematical functions because you can pass any number of values inside the array. so, for instance, this would throw an error:
-//logPersonName2.apply(person, 'en', 'es');
-
-//but this works
-logPersonName2.apply(person, ['en', 'es']);
-
-
-// immediately invoking with apply()
-
-(function(lang1, lang2) {
-    console.log('Logged: ' + this.getFullName());
-    console.log('Arguments: ' + lang1 + ' ' + lang2);
-    console.log('---------');
-}).apply(person, ['en', 'es']);
-
-/////////////////////////////////////////////////////
-// When would you actually ever use this?
-
-// function borrowing
-
-var person2 = {
-    firstname: 'Jane', 
-    lastname: 'Doe'
-}
-
-console.log(person.getFullName.apply(person2));
-
-// function currying
-// creating a copy of a function but with some preset parameters
-
-function multiply(a,b) {
-    return a*b;
-}
-
-// since bind() creates a copy of the method, this next line permanently sets the value of the a paramter when the copy is made. 
-var multiplyByTwo = multiply.bind(this, 2);
-
-console.log(multiplyByTwo(4));
-
+// when doing functional programming, you're splitting your behavior down into smaller and smaller increments. because of this, it's important not to mutate data in those functions. instead, you want to return a new result set. (guess that's why LINQ always returns a new collection instead of mutating the one passed to it.)
