5a3716a2e07dc888514587d17c234a38b86a257a
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-19 12:55:41 -0500

    Lecture 54 - Understanding the prototype

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-// talks about underscore.js 
-// can go to http://underscorejs.org/ and click "Annotated Source" at the top left to see the comments next to the code. 
+// inheritance: one object gets access to the properties and methods of another object
+// classical inheritance: whats in C#, Java, etc. negatives: it is verbose. 
+// prototypal inheritance: simple, flexible, extensible, easy to understand
 
-// he also says lodash.js is another good library to look through.
-// it is simlar to underscore.js but adds functionality
-// http://lodash.com
+// All objects (including functions) have a prototype property. Its simply a reference to another object we'll call proto. It stands alone, you could use it by itself if you wanted to. The prototype object can also have its own prototype. This is the Prototype Chain. 
+// Different objects can share the same prototype if you want them to. 
 
-var arr1 = [1, 2, 3];
+var person = { 
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;
+        // "this" refers to whatever object originated the call. john in the case below.
+    }
+}
 
-var arr6 = _.map(arr1, function(item) { return item * 3 });
-console.log(arr6);
+var john = { 
+    firstname: 'John',
+    lastname: 'Doe'
+}
 
-var arr7 = _.filter([2,3,4,5,6,7], function(item) { return item % 2 === 0; });
-console.log(arr7);
+//lets do it wrong first, don't ever do this because modern browsers do provide a way to directly access the prototype but this can cause a performance problem. 
+
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
+
+var jane = {
+    firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+console.log(jane.getFullName());
