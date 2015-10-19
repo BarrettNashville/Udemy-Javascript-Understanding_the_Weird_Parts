df197ebcf5a4edc58aa838ac57e3fbf113fdcb80
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-19 11:56:19 -0500

    Lecture 50: call(), apply(), bind(), function currying, function borrowing

--- app.js
+++ app.js
@@ -1,38 +1,84 @@
-function sayHiLater() {
-    var greeting = 'Hi';
-    
-    setTimeout(function() {
-        console.log(greeting);
-    }, 3000);
+var person = {
+    firstname: 'John',
+    lastname: 'Doe', 
+    getFullName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
 }
 
-sayHiLater();
-
-// sayHiLater has already been finished for 3 seconds when the anonymous callback function passed to setTimeout is triggered, but it still has access to the greeting variable set by sayHiLater
-
-// jQuery uses function expressions and first-class functions
-//$("button").click(function() {
-//    
-//});
-
-// callback function: I executed you, and you execute this other function for me when you're done. 
-// another definition: a function you give to another function, to be run when the other function is finished.
-// so the fucntion you call (invoke), 'calls back' by calling the function you gave it when it finishes
-
-// here is a more simplified example:
-
-function tellMeWhenDone(callback) {
-    var a = 1000; // some work
-    var b = 2000; // some more work
-    
-    callback(); // the 'callback', it runs the function i give it
+var logName = function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+    // fails if you call logname(); directly because "this" is pointing to the global object
 }
 
-tellMeWhenDone(function() {
-    console.log('I am done!');
-});
+var logPersonName = logName.bind(person);
+// there are three built in methods available to every function object: call(), apply(), and bind()
+// bind() allows you to pass whatever object you want to be the "this" variable when you run the function. the bind() method returns a new function, it actually makes a copy of the logName function in this case and does some special stuff behind the scenes so that "this" points by reference to the passed object
 
-tellMeWhenDone(function() {
-    alert('I am done!');
-});
+logPersonName();
+
+//you can immediately call the bind method when assigning the function to the variable
+
+var logName2 = function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+}.bind(person);
+
+logName2();
+
+
+//
+
+var logName3 = function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+    console.log('Arguments: ' + lang1 + ' ' + lang2);
+    console.log('---------');
+}
+
+var logPersonName2 = logName3.bind(person);
+
+logPersonName2('en');
+
+// call() calls the function, but it lets you bind to the "this" variable using the parameter passed. bind creates a copy of the function with the "this" variable bound, but it does not call the function. 
+
+logPersonName2.call(person, 'en', 'es');
+
+// apply() is the same as call() except it wants an array of parameters. thats the only difference. this can be useful for mathematical functions because you can pass any number of values inside the array. so, for instance, this would throw an error:
+//logPersonName2.apply(person, 'en', 'es');
+
+//but this works
+logPersonName2.apply(person, ['en', 'es']);
+
+
+// immediately invoking with apply()
+
+(function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+    console.log('Arguments: ' + lang1 + ' ' + lang2);
+    console.log('---------');
+}).apply(person, ['en', 'es']);
+
+/////////////////////////////////////////////////////
+// When would you actually ever use this?
+
+// function borrowing
+
+var person2 = {
+    firstname: 'Jane', 
+    lastname: 'Doe'
+}
+
+console.log(person.getFullName.apply(person2));
+
+// function currying
+// creating a copy of a function but with some preset parameters
+
+function multiply(a,b) {
+    return a*b;
+}
+
+// since bind() creates a copy of the method, this next line permanently sets the value of the a paramter when the copy is made. 
+var multiplyByTwo = multiply.bind(this, 2);
+
+console.log(multiplyByTwo(4));
 
