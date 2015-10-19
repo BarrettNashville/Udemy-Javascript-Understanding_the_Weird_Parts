9da25e1a777afd4a1e7eb136cd4598eb77c36aba
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-18 19:27:23 -0500

    Lecture 45: Passing global to an IIFE to overwrite a global variable

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
-// function statement
-function greet(name) {
-    console.log('Hello ' + name);
-}
-greet('John');
+var greeting = 'Hola';
 
-// using a function expression
-var greetFunc = function(name) {
-    console.log('Hello ' + name);
-};
-greetFunc('John');
-
-// using an Immediately Invoked Function Expression (IIFE)
-var greeting = function(name) {
-    console.log('Hello ' + name);   
-}();
-
-var greeting2 = function(name) {
-    return 'Hello ' + name;   
-};
-
-// this just returns the function definition
-console.log(greeting2);
-// invoke like so
-console.log(greeting2('John'));
-
-// now lets invoke it immediately so it returns the value to the variable, instead of returning the function object
-var greeting3 = function(name) {
-    return 'Hello ' + name;   
-}('John');
-
-console.log(greeting3);
-
-// The typical syntax for an IIFE
-
-var firstname = 'John';
-
-(function(name) {
-    var greeting = 'Inside IIFE: Hello';
+(function(global, name) {
+    var greeting = 'Hello';
+    global.greeting = 'Hello'
     console.log(greeting + ' ' + name);  
-})(firstname);
+})(window, 'John');
 
+console.log(greeting);
+
+// this is a way of intentionally overwriting the global "greeting" variable. You pass a reference to the gloabl object to your function and then overwrite the variable in there.
 
 
