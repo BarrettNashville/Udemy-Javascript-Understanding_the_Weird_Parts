f6052d18b9742ce6c39efedd26d7bbe270af63d5
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-19 21:11:14 -0500

    Lecture 59: The problem with forgetting the "new" keyword

--- app.js
+++ app.js
@@ -7,37 +7,21 @@ function Person(firstname, lastname) {
     
 }
 
-var john = new Person('John', 'Doe')
+// if you forget to add the "new" keyword, it causes problems
+// to prevent this, we use a capital letter for the name of the function that is going to be a function constructor. That way, if you have an error and look through your code and see a function that starts with a capital letter without the "new" keywowrd in front of it, then you probably forgot to add it.
+// there are new ways to create objects coming in new versions of JS, so this way of creating objects will likely eventually not be used
+var john = Person('John', 'Doe')//new Person('John', 'Doe')
 console.log(john);
 
 var jane = new Person('Jane', 'Doe')
 console.log(jane);
 
-// how do we set the prototype? well, when you use the function constructor, it sets the prototype for you.
-// john.__proto__ returns Person {}
-// any time you create a function object, they all get a prototype member. It is used ONLY by the new operator. It is only used when you are using a function as a function constructor
-
-// The prototype property on a function is not the prototype OF the function, its the prototype of any objects created if you're using the function as a function constructor
-// It starts its life as an empty object
-
 Person.prototype.getFullName = function() {
     return this.firstname + ' ' + this.lastname;
 }
-
-// this prototype property of all functions is where the prototype chain points for any objects created using that function as a constructor
-
-// you can add members to the prototype of the FUNCTION on the fly later after any objects have been created by that function constructor, and all those created objects will have access to that new member immediately
-
-// this means you can add features to ALL objects created by a function constructor at once AFTER they have already been created 
 
 Person.prototype.getFormalFullName = function() {
     return this.lastname + ' ' + this.firstname;
 }
 
 console.log(john.getFormalFullName());
-
-// in a lot of good JS libraries, properties are set up inside function constructors but methods are sitting on the prototype
-// why not add methods inside function constructor
-// anything added to objects take up memory space
-// every copy gets a copy of the function unless you add it to the prototype
-// each object will have different values, and thus will need different copies, of each property. but they will ALL use the SAME copy of each method
