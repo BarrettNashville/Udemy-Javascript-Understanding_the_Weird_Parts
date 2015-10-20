1f9f7459722e02e3bdecd321e428ed4060589822
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-19 21:43:13 -0500

    Lecture 62: Why not to use for...in... with arrays

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// explains the following using the console window
-/*
-var a = 3
-undefined
-var b = new Number(3)
-undefined
-a == b
-true
-//this coerces types
-undefined
-a === b
-false
-*/
+// Dangerous aside about arrays and for...in...
 
-// recommends moment.js which wraps up lots of functionality for dealing with dates; adding days etc. 
+var arr = ['John', 'Jane', 'Jim'];
 
-// this works because the "new" keyword isn't being used:
-/*
-var c = Number("3")
-undefined
-c
-3
-c === 3
-true
-*/
+for(var prop in arr) {
+    console.log(prop + ': ' + arr[prop]);
+}
+
+// arrays in JS are a little different than they are in other languages
+// arrays have name/value pairs. there are 3 properties on the array above. but this causes a problem if a framework adds features to the prototype of Arrays.
+
+console.log('---------');
+
+Array.prototype.myCustomFeature = 'cool!';
+
+var arr2 = ['John', 'Jane', 'Jim'];
+
+for(var prop in arr2) {
+    console.log(prop + ': ' + arr2[prop]);
+}
+
+// in the case of arrays, DON'T use for...in... . Use instead, the standard for(var i = 0; i < arr.length; i++) syntax
