614120bdc9398d4bc72a7b5fb6aae8e388fc4b8c
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-20 20:22:45 -0500

    Lecture 67: typeof and instanceof

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-var people = [
-    {
-        // the 'john' object
-        firstname: 'John',
-        lastname: 'Doe',
-        addresses: [
-            '111 Main St.',
-            '222 Third St.'
-        ]
-    },
-    {
-        // the 'jane' object
-        firstname: 'Jane',
-        lastname: 'Doe',
-        addresses: [
-            '333 Main St.',
-            '444 Third St.'
-        ],
-        greet: function() {
-            return 'Hello!';
-        }
-    }
-]
+var a = 3;
+console.log(typeof a); // lowercase "number" output, not "Number" which is an object
 
-console.log(people);
+var b = "Hello";
+console.log(typeof b); // string
 
-// this type of complex initialization can look overwhelming, but 
-// it is good for things like mocking
+var c = {};
+console.log(typeof c); // object
+
+var d = [];
+console.log(typeof d); // weird! returns "object"
+console.log(d.toString());  // returns an empty string "" because it tries to convert the contents of the array
+console.log(Object.prototype.toString.call(d)); // better!
+// .call() means: invoke this function but tell it what the "this" variable should point to
+
+function Person(name) {
+    this.name = name;
+}
+
+var e = new Person('Jane');
+console.log(typeof e); // object
+console.log(e instanceof Person); // going down the prototype chain of e, do you find "Person"? yes
+
+console.log(typeof undefined); // makes sense
+console.log(typeof null); // a bug since, like, forever...
+
+var z = function() { };
+console.log(typeof z);
+
