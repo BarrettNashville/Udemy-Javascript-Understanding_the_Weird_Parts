a0cd8b13e8876ad6fe81cd3daae399fe1e69e611
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-21 19:11:14 -0500

    Lecture 75: Greetr constructor and attaching it to the global object

--- Greetr.js
+++ Greetr.js
@@ -1,5 +1,28 @@
 (function(global, $) {
     
+    var Greetr = function(firstname, lastname, language) {
+        
+        // return an object created by a call to a constructor so that you don't have to use the "new" keyword everytime you invoke Greetr
+        return new Greetr.init(firstname, lastname, language);
+    }
     
+    // make the prototype a new empty object
+    Greetr.prototype = {};
+    
+    // create a constructor function. use "self" to maintain a consistent reference to "this" in case you use an inner function that points to the global object
+    Greetr.init = function(firstname, lastname, language) {
+        
+        var self = this;
+
+        self.firstname = firstname || '';
+        self.lastname = lastname || '';
+        self.language = language || 'en';
+    }
+    
+    // now each object created with the Greetr.init constructor function has a prototype that points the same prototype: Greetr.prototype. Otherwise, each object created by the Greetr.init constructor would get its own unique prototype object
+    Greetr.init.prototype = Greetr.prototype;
+    
+    // expose the Greetr object to the outside world; aka attach it to the global object. we're not going to worry about checking to see whether or not it exists. we also want to alias it with G$.
+    global.Greetr = window.G$ = Greetr;
     
 })(window, jQuery);
--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-// We're going to build a library/framework called GREETR. 
-// When given a first name, last name, and optional language, it generates formal and informal greetings. 
-// We want to support English and Spanish languages. 
-// We want an easy-to-type structure
-// Should also support jQuery. Give it jQuery object and it will fill that object with the greeting. 
-
+var g = G$('John', 'Doe');
+console.log(g);
